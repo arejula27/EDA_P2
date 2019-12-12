5195d95f299dc0586ab47cde9f80ad5868d27f3e
Íñigo Aréjula Aísa <arejula27@gmail.com>
2019-12-12 11:06:32 +0100

    coltest avanzado

--- test/coltest.cpp
+++ test/coltest.cpp
@@ -25,11 +25,12 @@ using namespace std;
  *****/
 
 //creo el dato de tipo pila
- coleccion<int,int> c;
+ 
 
 //Creo un test con titulo,subtitulo
-TEST(CreacionPila, crear)
+TEST(Coleccion, crear)
 {
+    coleccion<int, int> c;
     EXPECT_NO_FATAL_FAILURE(crear(c););
     EXPECT_FALSE(existe(c, 1));
     EXPECT_NO_FATAL_FAILURE(quitarRep(c,1));
@@ -40,6 +41,156 @@ TEST(CreacionPila, crear)
     EXPECT_EQ(0, val);
 }
 
+TEST(Introducir, positivos)
+{
+    int dat,num;
+    coleccion<int, int> c;
+    EXPECT_NO_FATAL_FAILURE(crear(c););
+    EXPECT_NO_FATAL_FAILURE(introducir(c,1,1,1));
+    EXPECT_TRUE(existe(c,1));
+    EXPECT_NO_FATAL_FAILURE(agnadirRep(c,1));
+    obtenerInfo(c,1,dat,num);
+    EXPECT_EQ(2,num);
+    numClaves(c,num);
+    EXPECT_EQ(1,num);
+    numCardinal(c,num);
+    EXPECT_EQ(2, num);
+
+    EXPECT_NO_FATAL_FAILURE(introducir(c, 2, 1, 1));
+    EXPECT_TRUE(existe(c, 1));
+    EXPECT_TRUE(existe(c, 2));
+    EXPECT_NO_FATAL_FAILURE(agnadirRep(c, 2));
+    obtenerInfo(c, 2, dat, num);
+    EXPECT_EQ(2, num);
+    numClaves(c, num);
+    EXPECT_EQ(2, num);
+    numCardinal(c, num);
+    EXPECT_EQ(4, num);
+
+    EXPECT_NO_FATAL_FAILURE(introducir(c, 3, 1, 1));
+    EXPECT_TRUE(existe(c, 1));
+    EXPECT_TRUE(existe(c, 2));
+    EXPECT_TRUE(existe(c, 3));
+    EXPECT_NO_FATAL_FAILURE(agnadirRep(c, 3));
+    obtenerInfo(c, 3, dat, num);
+    EXPECT_EQ(2, num);
+    numClaves(c, num);
+    EXPECT_EQ(3, num);
+    numCardinal(c, num);
+    EXPECT_EQ(6, num);
+}
+
+TEST(Introducir, cero)
+{
+    int dat, num;
+    coleccion<int, int> c;
+    EXPECT_NO_FATAL_FAILURE(crear(c););
+    EXPECT_NO_FATAL_FAILURE(introducir(c, 0, 1, 1));
+    EXPECT_FALSE(existe(c, 0));
+    EXPECT_NO_FATAL_FAILURE(agnadirRep(c, 0));
+    numClaves(c, num);
+    EXPECT_EQ(0, num);
+    numCardinal(c, num);
+    EXPECT_EQ(0, num);
+
+}
+
+TEST(Introducir, negativos)
+{
+    int dat, num;
+    coleccion<int, int> c;
+    EXPECT_NO_FATAL_FAILURE(crear(c););
+    EXPECT_NO_FATAL_FAILURE(introducir(c, -1, 1, 1));
+    EXPECT_FALSE(existe(c, 0));
+    EXPECT_NO_FATAL_FAILURE(agnadirRep(c, -1));
+    numClaves(c, num);
+    EXPECT_EQ(0, num);
+    numCardinal(c, num);
+    EXPECT_EQ(0, num);
+}
+
+TEST(Eliminar, eliminar_sin_hijos)
+{
+    int dat, num;
+    coleccion<int, int> c;
+    EXPECT_NO_FATAL_FAILURE(crear(c););
+    EXPECT_NO_FATAL_FAILURE(introducir(c, 2, 1, 1));
+
+
+
+    EXPECT_NO_FATAL_FAILURE(eliminar(c,2));
+    numClaves(c,num);
+    EXPECT_EQ(2,num);
+    EXPECT_FALSE(existe(c,2));
+}
+
+TEST(Eliminar, eliminar_solo_izq)
+{
+    int dat, num;
+    coleccion<int, int> c;
+    EXPECT_NO_FATAL_FAILURE(crear(c););
+    EXPECT_NO_FATAL_FAILURE(introducir(c, 2, 1, 1));
+    EXPECT_NO_FATAL_FAILURE(introducir(c, 1, 1, 1));
+    
+
+    EXPECT_NO_FATAL_FAILURE(eliminar(c, 2));
+    numClaves(c, num);
+    EXPECT_EQ(2, num);
+    EXPECT_FALSE(existe(c, 2));
+    EXPECT_TRUE(existe(c, 1));
+}
+
+TEST(Eliminar, eliminar_solo_der)
+{
+    int dat, num;
+    coleccion<int, int> c;
+    EXPECT_NO_FATAL_FAILURE(crear(c););
+    EXPECT_NO_FATAL_FAILURE(introducir(c, 2, 1, 1));
+    EXPECT_NO_FATAL_FAILURE(introducir(c, 3, 1, 1));
+
+    EXPECT_NO_FATAL_FAILURE(eliminar(c, 2));
+    numClaves(c, num);
+    EXPECT_EQ(2, num);
+    EXPECT_FALSE(existe(c, 2));
+    EXPECT_TRUE(existe(c, 3));
+}
+
+TEST(Eliminar, eliminar_dos_hijos)
+{
+    int dat, num;
+    coleccion<int, int> c;
+    EXPECT_NO_FATAL_FAILURE(crear(c););
+    EXPECT_NO_FATAL_FAILURE(introducir(c, 2, 1, 1));
+    EXPECT_NO_FATAL_FAILURE(introducir(c, 1, 1, 1));
+    EXPECT_NO_FATAL_FAILURE(introducir(c, 3, 1, 1));
+
+    EXPECT_NO_FATAL_FAILURE(eliminar(c, 2));
+    numClaves(c, num);
+    EXPECT_EQ(2, num);
+    EXPECT_FALSE(existe(c, 2));
+    EXPECT_TRUE(existe(c, 3));
+    EXPECT_TRUE(existe(c, 1));
+}
+
+TEST(quitarRep, no_se_elimina)
+{
+    int dat, num;
+    coleccion<int, int> c;
+    
+}
+
+TEST(quitarRep, elimina)
+{
+    int dat, num;
+    coleccion<int, int> c;
+}
+
+
+TEST(Iterador,iterador){
+
+    int dat, num;
+    coleccion<int, int> c;
+}
 
 
 int main(int argc, char **argv)
