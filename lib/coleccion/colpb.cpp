16d7653964b52a6a4b8255728c7c2689a8fab62f
Merni27 <marcoshernando123@gmail.com>
2019-12-14 13:08:30 +0100

    Cambios colpb

--- lib/coleccion/colpb.cpp
+++ lib/coleccion/colpb.cpp
@@ -11,10 +11,10 @@ int main(int argc, char **argv)
     crear(c);
     cout << "CREAR E INTRODUCIR" << endl;
     cout<<"----------------------------------"<<endl;
-    cout<<"EXISTE ? 0: "<<existe(c,2)<<endl;
+    cout<<"EXISTE(2) ? 0: "<<existe(c,2)<<endl;
     cout<<"INRODUCIR: "<<endl;
     introducir(c,5,5,1);
-    cout<<"EXISTE raiz ? 1: "<<existe(c,5)<<endl;
+    cout<<"EXISTE raiz(5) ? 1: "<<existe(c,5)<<endl;
     int num;
     numClaves(c,num);
     cout<<"CLAVES ?1: "<<num<<endl;
@@ -23,16 +23,13 @@ int main(int argc, char **argv)
 
     introducir(c,4,4,1);
     cout<<"INRODUCIR: izq "<<endl;
-    cout<<"EXISTE raiz ? 1: "<<existe(c,5)<<endl;
-    cout<<"EXISTE izq ? 1: "<<existe(c,4)<<endl;
+    cout<<"EXISTE raiz(5) ? 1: "<<existe(c,5)<<endl;
+    cout<<"EXISTE izq(4) ? 1: "<<existe(c,4)<<endl;
     numClaves(c,num);
     cout<<"CLAVES ?2: "<<num<<endl;
     numCardinal(c,num);
     cout<<"REPS ?2: "<<num<<endl;
     
-
-
-     introducir(c,6,6,1);
     cout<<"INRODUCIR: der "<<endl;
     introducir(c,6,6,2);
     
@@ -41,54 +38,52 @@ int main(int argc, char **argv)
     quitarRep(c,6);
     cout<<"REPS(der) ? 0: ";
     quitarRep(c,6);
-    cout<<"EXISTE raiz ? 1: "<<existe(c,5)<<endl;
-    cout<<"EXISTE izq ? 1: "<<existe(c,4)<<endl;
-    cout<<"EXISTE der ? 1: "<<existe(c,6)<<endl;
+    cout<<"EXISTE raiz(5) ? 1: "<<existe(c,5)<<endl;
+    cout<<"EXISTE izq(4) ? 1: "<<existe(c,4)<<endl;
+    cout<<"EXISTE der(6) ? 0: "<<existe(c,6)<<endl;
     numClaves(c,num);
-    cout<<"CLAVES ?3: "<<num<<endl;
+    cout<<"CLAVES ?2: "<<num<<endl;
     numCardinal(c,num);
-    cout<<"REPS ?3: "<<num<<endl;
+    cout<<"REPS ?2: "<<num<<endl;
 
-    introducir(c,10,6,1);
+    introducir(c,10,6,2);
     cout<<"INRODUCIR: der der"<<endl;
-    cout<<"EXISTE raiz ? 1: "<<existe(c,5)<<endl;
-    cout<<"EXISTE izq ? 1: "<<existe(c,4)<<endl;
-    cout<<"EXISTE der ? 1: "<<existe(c,6)<<endl;
-    cout<<"EXISTE der ? 1: "<<existe(c,10)<<endl;
+    cout<<"EXISTE raiz(5) ? 1: "<<existe(c,5)<<endl;
+    cout<<"EXISTE izq(4) ? 1: "<<existe(c,4)<<endl;
+    cout<<"EXISTE der(6) ? 0: "<<existe(c,6)<<endl;
+    cout<<"EXISTE der(10) ? 1: "<<existe(c,10)<<endl;
 
     cout<<"-------------------------------------\n";
     numClaves(c,num);
-    cout<<"CLAVES ?4: "<<num<<endl;
+    cout<<"CLAVES ?3: "<<num<<endl;
     numCardinal(c,num);
     cout<<"REPS ?4: "<<num<<endl;
 
     introducir(c,0,6,1);
     introducir(c,-8,6,1);
     cout<<"INRODUCIR: der der"<<endl;
-    cout<<"EXISTE raiz ? 1: "<<existe(c,5)<<endl;
-    cout<<"EXISTE izq ? 1: "<<existe(c,4)<<endl;
-    cout<<"EXISTE der ? 0: "<<existe(c,6)<<endl;
-    cout<<"EXISTE der ? 1: "<<existe(c,10)<<endl;
-    cout<<"EXISTE der ? 0: "<<existe(c,0)<<endl;
-    cout<<"EXISTE der ? 0: "<<existe(c,-8)<<endl;
+    cout<<"EXISTE raiz(5) ? 1: "<<existe(c,5)<<endl;
+    cout<<"EXISTE izq(4) ? 1: "<<existe(c,4)<<endl;
+    cout<<"EXISTE der(6) ? 0: "<<existe(c,6)<<endl;
+    cout<<"EXISTE der(10) ? 1: "<<existe(c,10)<<endl;
+    cout<<"EXISTE izd(0) ? 1: "<<existe(c,0)<<endl;
+    cout<<"EXISTE izd(-8) ? 1: "<<existe(c,-8)<<endl;
     numClaves(c,num);
-    cout<<"CLAVES ?4: "<<num<<endl;
+    cout<<"CLAVES ?5: "<<num<<endl;
     numCardinal(c,num);
-    cout<<"REPS ?4: "<<num<<endl;
+    cout<<"REPS ?6: "<<num<<endl;
 
-    cout <<  endl;
-
-    cout << "AÑADIR REPS" << endl;
-    cout<<"-------------------------------------"<<endl;
+    //PARTE DE IÑIGO
+    
     agnadirRep(c,5);
-    cout<<"AÑADO raiz "<<endl;
+    cout<<"AÑADO raiz(5) "<<endl;
     numClaves(c,num);
-    cout<<"CLAVES ?4: "<<num<<endl;
+    cout<<"CLAVES ?5: "<<num<<endl;
     numCardinal(c,num);
-    cout<<"REPS ?5: "<<num<<endl;
+    cout<<"REPS ?7: "<<num<<endl;
 
     agnadirRep(c,4);
-    cout<<"AÑADO izq : "<<endl;
+    cout<<"AÑADO izq(4) : "<<endl;
     numClaves(c,num);
     cout<<"CLAVES ?4: "<<num<<endl;
     numCardinal(c,num);
